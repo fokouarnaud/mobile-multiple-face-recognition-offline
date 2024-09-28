45eac1d89dc214e62d37116821d82464414276c0
fokouarnaud <fokouarnaud@gmail.com>
2024-09-28 23:12:25 +0200

    init commit last

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutterface/ui/home_page.dart';
 import 'package:logging/logging.dart';
 
+/// Configures logging for the application.
+///
+/// Sets the root logger level to capture all log messages (INFO, DEBUG, etc.)
+/// and redirects them to the developer tools for display.
 void main() async {
   // Set up logging
   Logger.root.level = Level.ALL; // Capture all log levels
@@ -16,6 +20,11 @@ void main() async {
   runApp(const MyApp());
 }
 
+/// The main application widget.
+///
+/// This widget defines the root of the application's widget tree.
+/// It uses the MaterialApp widget to configure the application's title, theme,
+/// and home screen.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
